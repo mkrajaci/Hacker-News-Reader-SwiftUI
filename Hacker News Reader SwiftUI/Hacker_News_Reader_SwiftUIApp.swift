b5b7a1effd7c27c6e8878fc4ad65a352eee38570
Mario Krajacic <krajacic.mario@gmail.com>
2023-04-08 17:01:24 +0200

    Initial Commit

---
+++ Hacker News Reader SwiftUI/Hacker_News_Reader_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_Reader_SwiftUIApp.swift
+//  Hacker News Reader SwiftUI
+//
+//  Created by Mario Krajačić on 08.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_Reader_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
